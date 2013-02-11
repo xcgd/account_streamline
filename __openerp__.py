e5e381f535545900b7b7d9876e5aa71a70ba2db6
Florent Aide, <florent.aide@gmail.com>
2013-02-11 14:58:15 +0100

    initial import

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+{
+    "name" : "account_move_line_listing",
+    "version" : "0.1",
+    "author" : "XCG Consulting",
+    "category": 'Accounting',
+    "description": """Enhancements to the account_move_line default view
+    """,
+    'website': 'http://www.openerp-experts.com',
+    'init_xml': [],
+    "depends" : ['base', 'account', ],
+    "data": [
+        'account_move_line.xml',
+    ],
+    #'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+    #'certificate': '0080331923549',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
