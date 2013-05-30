2281db8729930c4811e74817fb4daf51a4a3b3d1
nhe <nicolas.he@xcg-consulting.fr>
2013-05-30 11:51:15 +0200

    added a class which represents a filter, added a listbox (many2one field) on the filter type (onchange id not implemented yet)

--- wizard/account_reconcile_filter.py
+++ wizard/account_reconcile_filter.py
@@ -6,6 +6,11 @@ class account_reconcile_filter(osv.TransientModel):
         'obj_list' : fields.integer('Un entier'),
         'id_from' : fields.integer('From'),
         'id_to' : fields.integer('To'),
+	'filter_type' : fields.many2one('account.streamline.filter', 'Search by'),
+        'search_type' : fields.char('search by', name="Search", method=True),
+        'account_from' : fields.char('account_from'),
+        'account_to' : fields.date('account_to'),
+
     }
     def launch_search(self, cr, uid, ids, context=None):
         filter_ids = self.browse(cr, uid, ids, context=context)
@@ -23,4 +28,10 @@ class account_reconcile_filter(osv.TransientModel):
         print args
         res = {}
         return res
+
+class account_filter(osv.TransientModel):
+    _name = 'account.streamline.filter'
+    _columns = {
+	'name' : fields.char('Filtre', size=250),
+     }
 
