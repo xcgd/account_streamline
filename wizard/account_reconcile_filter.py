a3565cc92d46e14b69c575ca83147a6d7bc60ba2
alorimier <anael.lorimier@xcg-consulting.fr>
2013-05-31 14:52:48 +0200

    See #2 - Adding fields_view_get override to future enhancements

--- wizard/account_reconcile_filter.py
+++ wizard/account_reconcile_filter.py
@@ -2,32 +2,33 @@ from openerp.osv import fields, osv
 
 class account_reconcile_filter(osv.TransientModel):
     _name = 'account.streamline.reconcile_filter'
+
     _columns = {
-        'id_from'       : fields.many2one('account.account', 'From'),
-        'id_to'         : fields.many2one('account.account', 'To'),
-	    'filter_type'   : fields.many2one('account.streamline.filter', 'Search by'),
-        'search_type'   : fields.char('search by', name="Search", method=True),
+        'id_from'       : fields.char('From', size=256),
+        'id_to'         : fields.char('To', size=256),
+        'field_filter'  : fields.many2one('ir.model.fields', 'Search Field'),
     }
 
     def launch_search(self, cr, uid, ids, context=None):
         filter_ids = self.browse(cr, uid, ids, context=context)
         args = dict()
 
-        for filter_id in filter_ids:
-            args['obj_type'] = 'account.move.line'
-            args['field_filter'] = filter_id.search_type
-            args['id_filter_from'] = filter_id.id_from.id
-            args['id_filter_to'] = filter_id.id_to.id
-            adv_filter_osv = self.pool.get("account.streamline.advanced_filter")
-            if adv_filter_osv:
-                adv_filter_osv.create_new_filter(cr, uid,
-                                                 ids, args=args,
-                                                 context=context)
+        args['obj_type'] = 'account.move.line'
+        args['field_filter'] = filter_ids[0].field_filter.id
+        args['id_filter_from'] = filter_ids[0].id_from
+        args['id_filter_to'] = filter_ids[0].id_to
+        adv_filter_osv = self.pool.get("account.streamline.advanced_filter")
+        if adv_filter_osv:
+            adv_filter_osv.create_new_filter(cr, uid,
+                                             ids, args=args,
+                                             context=context)
         return {'type': 'ir.actions.act_window_close'}
 
-class account_filter(osv.TransientModel):
-    _name = 'account.streamline.filter'
-    _columns = {
-        'name' : fields.char('Filtre', size=250),
-     }
+    def fields_view_get(self, cr, uid, view_id=None,
+                        view_type='form', context=None,
+                        toolbar=False):
+        result = super(account_reconcile_filter,
+                       self).fields_view_get(cr, uid, view_id, view_type,
+                                             context=context, toolbar=toolbar)
+        return result
 
