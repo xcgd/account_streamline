ed1a93346bca1496d1be20bf3e6010c225e4efba
alorimier <anael.lorimier@xcg-consulting.fr>
2013-06-13 18:20:50 +0200

    Remove data csv

--- __openerp__.py
+++ __openerp__.py
@@ -32,9 +32,6 @@
         'account_move_line_journal_items.xml',
         'account_move_line_journal_view.xml',
         'account_menu_entries.xml',
-        'data/analytic.code.csv',
-        'data/analytic.dimension.csv',
-        'data/analytic.structure.csv'
     ],
     #'demo_xml': [],
     'test': [],
