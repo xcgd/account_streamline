8b7da5c3a9b1b06b47115c9cd703d0a52b750103
Florent Aide <florent.aide@gmail.com>
2013-06-28 15:53:53 +0200

    Better protection in case the  res.partner has not account_payable or account_receivable property set on its form PEP8 fields_view_get implementation fix for better performance

--- partner.py
+++ partner.py
@@ -29,35 +29,49 @@ class res_partner_needaction(osv.Model):
     _inherit = ['res.partner', 'mail.thread', 'ir.needaction_mixin']
 
     def _check_supplier_account(self, cr, uid, ids, name, args, context=None):
+        account_osv = self.pool.get('account.account')
         res = dict()
         for partner in self.browse(cr, uid, ids, context=context):
-            account = self.pool.get('account.account').browse(cr, uid,
-                                                              partner.property_account_payable.id)
-            res[partner.id] = partner.supplier and \
-                              not account.type == 'payable'
+            account = account_osv.browse(
+                cr, uid, partner.property_account_payable.id)
+
+            payable = account.type == 'payable'
+            res[partner.id] = partner.supplier and not payable
+
         return res
 
     def _check_customer_account(self, cr, uid, ids, name, args, context=None):
+        account_osv = self.pool.get('account.account')
         res = dict()
         for partner in self.browse(cr, uid, ids, context=context):
-            account = self.pool.get('account.account').browse(cr, uid,
-                                                              partner.property_account_receivable.id)
-            res[partner.id] = partner.customer and \
-                              not account.type == 'receivable'
+            account = account_osv.browse(
+                cr, uid,
+                partner.property_account_receivable.id
+            )
+
+            receivable = account.type == 'receivable'
+            res[partner.id] = partner.customer and not receivable
+
         return res
 
     _track = {
         'property_account_payable': {
             'account_streamline.mt_partner_supplier': lambda self,
-                                                             cr, uid, obj, ctx=None:
-            not self.pool.get('account.account').browse(cr, uid,
-                                                        obj['property_account_payable']).type == 'payable',
+            cr, uid, obj, ctx=None:
+            not self.pool.get(
+                'account.account').browse(
+                    cr, uid,
+                    obj['property_account_payable']
+                ).type == 'payable',
         },
         'property_account_receivable': {
             'account_streamline.mt_partner_customer': lambda self,
-                                                             cr, uid, obj, ctx=None:
-            not self.pool.get('account.account').browse(cr, uid,
-                                                        obj['property_account_receivable']).type == 'receivable',
+            cr, uid, obj, ctx=None:
+            not self.pool.get(
+                'account.account').browse(
+                    cr, uid,
+                    obj['property_account_receivable']
+                ).type == 'receivable',
         },
     }
 
@@ -81,25 +95,46 @@ class res_partner_needaction(osv.Model):
             cr, uid, values, context=context)
 
         obj = self.pool.get('ir.model.data')
-        followers = [follower.id for follower in obj.get_object(cr, uid, 'account_streamline',
-                                   'group_account_creators').message_follower_ids]
-        msg_cust = obj.get_object(cr, uid, 'account_streamline', 'mt_partner_customer').id
-        msg_supp = obj.get_object(cr, uid, 'account_streamline', 'mt_partner_supplier').id
+        followers = [
+            follower.id for follower in obj.get_object(
+                cr, uid, 'account_streamline', 'group_account_creators'
+            ).message_follower_ids
+        ]
+        msg_cust = obj.get_object(
+            cr, uid,
+            'account_streamline', 'mt_partner_customer'
+        ).id
+
+        msg_supp = obj.get_object(
+            cr, uid,
+            'account_streamline', 'mt_partner_supplier'
+        ).id
+
         subtypes = [msg_cust, msg_supp]
 
         self.message_subscribe(cr, uid, [cur_id], followers, subtypes,
-                              context=context)
+                               context=context)
 
-        if values.get('supplier', None) and not self.pool.get('account.account').browse(cr, uid,
-                                                       values['property_account_payable']).type == 'payable':
+        account_osv = self.pool.get('account.account')
+        supplier = values.get('supplier', None)
+        customer = values.get('customer', None)
+        p_accnt_receivable = values.get('property_account_receivable', False)
+        p_accnt_payable = values.get('property_account_payable', False)
+
+        if supplier and p_accnt_payable and not account_osv.browse(
+            cr, uid, p_accnt_payable
+        ).type == 'payable':
+
             self.message_post(
                 cr, uid, cur_id,
                 subtype='account_streamline.mt_partner_supplier',
                 body=_("Supplier created, please check related account."),
                 context=context)
 
-        if values.get('customer', None) and not self.pool.get('account.account').browse(cr, uid,
-                                                       values['property_account_receivable']).type == 'receivable':
+        if customer and p_accnt_receivable and not account_osv.browse(
+            cr, uid, p_accnt_receivable
+        ).type == 'receivable':
+
             self.message_post(
                 cr, uid, cur_id,
                 subtype='account_streamline.mt_partner_customer',
@@ -116,7 +151,11 @@ class res_partner_needaction(osv.Model):
         dom = super(res_partner_needaction, self)._needaction_domain_get(
             cr, uid, context=context)
         obj = self.pool.get('ir.model.data')
-        followers = obj.get_object(cr, uid, 'account_streamline', 'group_account_creators').message_follower_ids
+        followers = obj.get_object(
+            cr, uid,
+            'account_streamline',
+            'group_account_creators'
+        ).message_follower_ids
 
         user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
 
@@ -130,60 +169,110 @@ class res_partner_needaction(osv.Model):
 
         return dom
 
+
 class res_partner(osv.Model):
     _inherit = 'res.partner'
 
     _columns = {
-        'a1_id':fields.many2one('analytic.code', "Analysis Code 1",
-                               domain=[('nd_id.ns_id.model_name', '=', 'res_partner'),
-                                       ('nd_id.ns_id.ordering', '=', '1')]),
-        'a2_id':fields.many2one('analytic.code', "Analysis Code 1",
-                               domain=[('nd_id.ns_id.model_name', '=', 'res_partner'),
-                                       ('nd_id.ns_id.ordering', '=', '2')]),
-        'a3_id':fields.many2one('analytic.code', "Analysis Code 1",
-                               domain=[('nd_id.ns_id.model_name', '=', 'res_partner'),
-                                       ('nd_id.ns_id.ordering', '=', '3')]),
-        'a4_id':fields.many2one('analytic.code', "Analysis Code 1",
-                               domain=[('nd_id.ns_id.model_name', '=', 'res_partner'),
-                                       ('nd_id.ns_id.ordering', '=', '4')]),
-        'a5_id':fields.many2one('analytic.code', "Analysis Code 1",
-                               domain=[('nd_id.ns_id.model_name', '=', 'res_partner'),
-                                       ('nd_id.ns_id.ordering', '=', '5')]),
+        'a1_id': fields.many2one(
+            'analytic.code', "Analysis Code 1",
+            domain=[
+                ('nd_id.ns_id.model_name', '=', 'res_partner'),
+                ('nd_id.ns_id.ordering', '=', '1'),
+            ]
+        ),
+        'a2_id': fields.many2one(
+            'analytic.code', "Analysis Code 1",
+            domain=[
+                ('nd_id.ns_id.model_name', '=', 'res_partner'),
+                ('nd_id.ns_id.ordering', '=', '2'),
+            ]
+        ),
+        'a3_id': fields.many2one(
+            'analytic.code', "Analysis Code 1",
+            domain=[
+                ('nd_id.ns_id.model_name', '=', 'res_partner'),
+                ('nd_id.ns_id.ordering', '=', '3'),
+            ]
+        ),
+        'a4_id': fields.many2one(
+            'analytic.code', "Analysis Code 1",
+            domain=[
+                ('nd_id.ns_id.model_name', '=', 'res_partner'),
+                ('nd_id.ns_id.ordering', '=', '4'),
+            ]
+        ),
+        'a5_id': fields.many2one(
+            'analytic.code', "Analysis Code 1",
+            domain=[
+                ('nd_id.ns_id.model_name', '=', 'res_partner'),
+                ('nd_id.ns_id.ordering', '=', '5'),
+            ]
+        ),
     }
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
-        if context is None:context = {}
-        res = super(res_partner, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
+                        context=None, toolbar=False, submenu=False):
+        if context is None:
+            context = {}
+
+        res = super(
+            res_partner, self
+        ).fields_view_get(
+            cr, uid, view_id=view_id,
+            view_type=view_type, context=context,
+            toolbar=toolbar, submenu=False
+        )
+
         ans_obj = self.pool.get('analytic.structure')
 
-        #display analysis codes only when present on a related structure, with dimension name as label
-        ans_ids = ans_obj.search(cr, uid, [('model_name', '=', 'res_partner')], context=context)
-        ans_br = ans_obj.browse(cr, uid, ans_ids,context=context)
+        #display analysis codes only when present on a related structure,
+        # with dimension name as label
+        ans_ids = ans_obj.search(
+            cr, uid,
+            [
+                ('model_name', '=', 'res_partner'),
+            ],
+            context=context
+        )
+
+        ans_br = ans_obj.browse(cr, uid, ans_ids, context=context)
         ans_dict = dict()
         for ans in ans_br:
             ans_dict[ans.ordering] = ans.nd_id.name
 
         doc = etree.XML(res['arch'])
 
-        for field in res['fields']:
-            if field == 'a1_id':
-                res['fields'][field]['string'] = ans_dict.get('1', 'A1')
-                doc.xpath("//field[@name='a1_id']")[0].set('modifiers', '{"invisible": %s}' % str(not '1' in ans_dict).lower())
-            if field == 'a2_id':
-                res['fields'][field]['string'] = ans_dict.get('2', 'A2')
-                doc.xpath("//field[@name='a2_id']")[0].set('modifiers', '{"invisible": %s}' % str(not '2' in ans_dict).lower())
-            if field == 'a3_id':
-                res['fields'][field]['string'] = ans_dict.get('3', 'A3')
-                doc.xpath("//field[@name='a3_id']")[0].set('modifiers', '{"invisible": %s}' % str(not '3' in ans_dict).lower())
-            if field == 'a4_id':
-                res['fields'][field]['string'] = ans_dict.get('4', 'A4')
-                doc.xpath("//field[@name='a4_id']")[0].set('modifiers', '{"invisible": %s}' % str(not '4' in ans_dict).lower())
-            if field == 'a5_id':
-                res['fields'][field]['string'] = ans_dict.get('5', 'A5')
-                #res['fields'][field]['invisible'] = not '5' in ans_dict
-                doc.xpath("//field[@name='a5_id']")[0].set('modifiers', '{"invisible": %s}' % str(not '5' in ans_dict).lower())
+        if 'a1_id' in res['fields']:
+            res['fields']['a1_id']['string'] = ans_dict.get('1', 'A1')
+            doc.xpath("//field[@name='a1_id']")[0].set(
+                'modifiers', '{"invisible": %s}' % str(
+                    not '1' in ans_dict).lower())
+
+        elif 'a2_id' in res['fields']:
+            res['fields']['a2_id']['string'] = ans_dict.get('2', 'A2')
+            doc.xpath("//field[@name='a2_id']")[0].set(
+                'modifiers', '{"invisible": %s}' % str(
+                    not '2' in ans_dict).lower())
+
+        elif 'a3_id' in res['fields']:
+            res['fields']['a3_id']['string'] = ans_dict.get('3', 'A3')
+            doc.xpath("//field[@name='a3_id']")[0].set(
+                'modifiers', '{"invisible": %s}' % str(
+                    not '3' in ans_dict).lower())
+
+        elif 'a4_id' in res['fields']:
+            res['fields']['a4_id']['string'] = ans_dict.get('4', 'A4')
+            doc.xpath("//field[@name='a4_id']")[0].set(
+                'modifiers', '{"invisible": %s}' % str(
+                    not '4' in ans_dict).lower())
+
+        if 'a5_id' in res['fields']:
+            res['fields']['a5_id']['string'] = ans_dict.get('5', 'A5')
+            doc.xpath("//field[@name='a5_id']")[0].set(
+                'modifiers', '{"invisible": %s}' % str(
+                    not '5' in ans_dict).lower())
 
         res['arch'] = etree.tostring(doc)
 
         return res
-
