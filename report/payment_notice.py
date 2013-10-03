280b97535775140acebf8752f76f22b2c09b2b99
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2013-10-03 19:31:51 +0200

    Add some features to mako report

--- report/payment_notice.py
+++ report/payment_notice.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from openerp import pooler
 from openerp.report import report_sxw
 from openerp.tools.translate import _
 from osv import osv
@@ -33,11 +34,13 @@ class payment_notice_parser(report_sxw.rml_parse):
         return amount.strip()
 
     def get_message(self, this_br):
-       company = this_br.partner_id.bank_ids.company_id
+       company = this_br.partner_id.bank_ids[0].company_id
+       if not company:
+           return ''
        if this_br.state == 'draft':
-           print company.message_voucher_draft
+           return company.message_voucher_draft
        if this_br.state == 'posted':
-           print company.message_voucher_validate
+           return company.message_voucher_validate
 
     def sepa_generated(self, this_br):
         if this_br.batch_id:
@@ -45,7 +48,24 @@ class payment_notice_parser(report_sxw.rml_parse):
         return False
 
 class payment_notice_report(WebKitParser):
-    pass
+    def remove_previous(self, cr, uid, ids, context=None):
+        # get attachement model
+        ir_att_osv = pooler.get_pool(cr.dbname).get('ir.attachment')
+        # previous ids
+        data_ids = ir_att_osv.search(
+            cr,
+            uid,
+            [('res_model', '=', 'account.voucher'),
+             ('res_id', 'in', ids)],
+            context=context)
+        # remove previous items
+        ir_att_osv.unlink(cr, uid, data_ids)
+
+    def create(self, cr, uid, ids, datas, context=None):
+        # remove previous items
+        self.remove_previous(cr, uid, ids, context=context)
+        # call parent
+        return super(payment_notice_report, self).create(cr, uid, ids, datas, context)
 
 
 payment_notice_report('report.account_streamline.payment_notice',
