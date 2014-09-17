17a1920f510f903ed4bedab9c2ca0cc500abcaa6
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-09-17 11:51:28 +0200

    Bumped to version 1.8.6 (dev).

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 ##############################################################################
 {
     "name": "Account Streamline",
-    "version": "1.8.5",
+    "version": "1.8.6",
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account module to streamline its
