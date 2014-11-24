865354fb5181bc335304811ce004f6795a47ea67
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-11-24 10:31:06 +0100

    Version 1.10.1

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 ##############################################################################
 {
     "name": "Account Streamline",
-    "version": "1.10",
+    "version": "1.10.1",
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account module to streamline its
