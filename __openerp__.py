66923d5f156c44e686e2eb757a802c11f1de8dfa
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2015-02-11 18:08:32 +0100

    Bumped to 1.14 (stable)

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Account Streamline",
-    "version": '1.13.1',
+    "version": '1.14',
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account module to streamline its
