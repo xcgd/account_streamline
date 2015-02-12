81de671ce7057d8add31ae9ede74f32c5e086b3c
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2015-02-12 10:23:59 +0100

    Update TAG to 1.14.3 (dev)

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Account Streamline",
-    "version": '1.14.2',
+    "version": '1.14.3',
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account module to streamline its
