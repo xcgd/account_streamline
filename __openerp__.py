6c1b21a384bae1a336f621cf4864876026134819
Vincent Hatakeyama <vincent.lhote@xcg-consulting.fr>
2015-03-02 16:24:28 +0100

    1.18

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Account Streamline",
-    "version": '1.17',
+    "version": '1.18',
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account module to streamline its
